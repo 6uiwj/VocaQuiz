4052f69e76bdafe7b2748d70e6aaf8a7e7fc49b6
6uiwj <oterges00@gmail.com>
2025-05-17 21:08:14 +0900

    250517 - 주석추가

--- src/main/resources/static/selectQuizDay.js
+++ src/main/resources/static/selectQuizDay.js
@@ -7,17 +7,17 @@ function selectQuizType(type) {
 document.addEventListener("DOMContentLoaded", function () {
  const buttons = document.querySelectorAll(".day-choose-button");
 
-    // 현재 URL에서 type 값을 가져옴
-    const urlParams = new URLSearchParams(window.location.search);
-    const type = urlParams.get("type");
+    // 현재 URL의 쿼리스트링에서 type 값을 가져옴
+    const urlParams = new URLSearchParams(window.location.search); //현재 URL의 쿼리스트링을 가져와서 키와 값 쌍으로 저장
+    const type = urlParams.get("type"); //저장된 쿼리스트링 요소 중 키가 type은 요소의 값 가져오기
 
-    buttons.forEach(button => {
+    buttons.forEach(button => { //각 날짜 버튼마다
         button.addEventListener("click", function (e) {
-            e.preventDefault();
-            const day = button.getAttribute("data-day");
+            e.preventDefault(); //클릭되면 링크 넘어가는 기본 a 태그 동작 제거
+            const day = button.getAttribute("data-day"); //html의 data-day 정보 받아오기
 
-            if (type && day) {
-                // 원하는 주소로 이동
+            if (type && day) { //퀴즈타입과 날짜가 존재하면
+                // 주소로 이동 -> 주소 /quiz/유형/날짜 형식으로 지정
                 window.location.href =  `/quiz/${type}/${day}`;
             } else {
                 alert("타입 또는 데이 값이 없습니다.");
